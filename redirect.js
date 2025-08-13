c1947cdde12704840999b5f87c0faf7e849bc6cc
LXS <111274267+ABCDCME@users.noreply.github.com>
2025-08-13 17:05:38 +0800

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -5,6 +5,7 @@
         const params = new URLSearchParams({
             userId: userId
         });
-        window.location.href = `https://help.sap.com/?${params.toString()}`;
+       // window.location.href = `https://help.sap.com/?${params.toString()}`;
+       window.location.href = `https://cockpit.hanatrial.ondemand.com/trial/#/globalaccount/f67dc1f7-2173-4297-904e-5e0c7348f212/?${params.toString()}`;
     }
-);
+)();
