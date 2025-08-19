696f44be698587ba2a5f0b7d69b677080d18b408
LXS <111274267+ABCDCME@users.noreply.github.com>
2025-08-19 10:32:05 +0800

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,6 @@
+// 当前页面：存储数据
+localStorage.setItem('userID', JSON.parse(window['@xweb/core-utils/src/util/templateVariable/variables'].pageMetaAttributes).userId);
+//sessionStorage.setItem('token', window.authToken);
+
+// 跳转
+window.location.href = 'https://6002bdc8trial.launchpad.cfapps.us10.hana.ondemand.com/8fb4f020-cc8b-45df-a772-627d7ab9b9d2.zbmsf002.comdeloittecnco001zbuco001-0.0.1/index.html';
