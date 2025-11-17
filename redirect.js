28ea6bfdfc2fceb240f92b95b403c7172a56e7db
LXS <111274267+ABCDCME@users.noreply.github.com>
2025-11-17 10:28:38 +0800

    Refactor redirect logic to use template variables

--- redirect.js
+++ redirect.js
@@ -1,18 +1,38 @@
-(function() {
-    // 登録ユーザーIDの取得
-    let userID = JSON.parse(window['@xweb/core-utils/src/util/templateVariable/variables'].pageMetaAttributes).userId;
-    // ターゲットURLパラメータの作成
-    const params = new URLSearchParams({
-            userID: userID
-        });
+(function () {
     try {
-    // 画面遷移のターゲットURLの作成
-    const redirectUrl = `https://mol-dev-j4jvgqnp.launchpad.cfapps.eu10.hana.ondemand.com/01740d26-bdfb-43e7-972b-ec9fff7502ca.zbasfhe001.jpcommolzbusfhe001001zbusfhe001001-0.0.1/index.html?#/talentList?lang=en_us&type=1&${params.toString()}`;
-    window.open(redirectUrl, '_blank');
+        // 安全にテンプレート変数を取得
+        const templateVar = window['@xweb/core-utils/src/util/templateVariable/json'];
+        const pageMeta = templateVar && templateVar.pageMetaAttributes;
+        const userId = (pageMeta && pageMeta.userId) || '';
+        const userLocale = (pageMeta && pageMeta.userLocale) || '';
+        const languageCode = userLocale ? String(userLocale).split('_')[0] : '';
+
+        // クエリパラメータの生成
+        const params = new URLSearchParams({
+            loginId: userId,
+            lang: languageCode
+        });
+
+        // JavaScriptファイルのURL取得
+        let currentScript = document.currentScript;
+        // document.currentScript が null になる環境への降級処理
+        if (!currentScript) {
+            const scripts = document.getElementsByTagName('script');
+            currentScript = scripts[scripts.length - 1];
+        }
+        // URLとpageIdの取得
+        const src = (currentScript && currentScript.src) ? currentScript.src : '';
+        const url = src ? new URL(src, location.href) : null;
+        const pageId = url ? url.searchParams.get('page') : null;
+
+        // リダイレクト先URL の作成（pageId がない場合はデフォルトルートにする）
+        const base = 'https://mol-dev-j4jvgqnp.launchpad.cfapps.eu10.hana.ondemand.com/01740d26-bdfb-43e7-972b-ec9fff7502ca.zbasfhe001.jpcommolzbusfhe001001zbusfhe001001-0.0.1/index.html';
+        const hash = pageId ? `#/${pageId}?${params.toString()}&type=1` : `#/?${params.toString()}&type=1`;
+        const redirectUrl = `${base}${hash}`;
+
+        // 新しいタブで開く
+        window.open(redirectUrl, '_blank');
     } catch (error) {
-        console.error("Redirect error:", error);
+        console.error('Redirect error:', error);
     }
-}
-)();
-
-
+})();
